728476c77f7509e9682bd6ffcb0a756690f2677f
Rowland <rowlandiyoyo@gmail.com>
2022-09-09 13:35:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * * main - Entry point
- * * Description: prints "\"Programming is like building a multilingual puzzle"
- * * Return: 0
- * */
-
+ *  * Description: main - Prints out a string to stdout.
+ *   * Return: 0 if success.
+ *    */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(Mystr);
+				return (0);
 }
